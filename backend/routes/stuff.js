784dc09c32dab86145ece26ab914a9bf65510fb5
Nicolas Demangeat <demangeatn@gmail.com>
2022-12-01 18:41:53 +0100

    init commit

---
+++ backend/routes/stuff.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer.config')
+
+const router = express.Router();
+
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+//router.post('/:id/like', auth, stuffCtrl.createThing);
+
+module.exports = router;
